08f78d69fcb7d67cbadfc9042f28dcc29f958ad4
Jose Urena <urena.jose@gmail.com>
2020-10-01 23:26:35 -0400

    update

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ bitterness = go.Bar(
     x=beers,
     y=ibu_values,
     name=label1,
-    marker={'color':color1}
+    marker={'color':color1},
+    orientation='h'
 )
 alcohol = go.Bar(
     x=beers,
     y=abv_values,
     name=label2,
-    marker={'color':color2}
+    marker={'color':color2},
+    orientation='h'
 )
 
 beer_data = [bitterness, alcohol]
